1ce872c91171cf6497651f82b7f074311d02952d
Cedric Ko <yacmov@gmail.com>
2024-12-12 20:15:00 +0800

    feat: simple searchable movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_KEY}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('spiderman');
+  }, []);
 
   return (
-    <div className='App'>
-      <h1>App</h1>
+    <div className='app'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input
+          placeholder={'Search for movies'}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movie found</h2>
+        </div>
+      )}
     </div>
   );
 };
